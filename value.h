4854911b29696a712516ad1eb3a9eb686450829d
AkolenDvdnk <ak0lenfs@gmail.com>
2022-11-15 16:24:59 +0100

    added a new struct "ValueArray "to store the chunks constants
    also added a few funcs to init, write and free ValueArray

---
+++ value.h
@@ -0,0 +1,18 @@
+#ifndef persimmon_value_h
+#define persimmon_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct{
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array, Value value);
+
+#endif
