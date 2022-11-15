85b9d8d92aa39da53ba25d12c115c94b89865950
AkolenDvdnk <ak0lenfs@gmail.com>
2022-11-15 15:46:21 +0100

    disassembling chunks

---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef persimmon_debug_h
+#define persimmon_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
