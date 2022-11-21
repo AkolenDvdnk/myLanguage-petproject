a94435554e43a9f4c6182c9706d10f2e564e43af
AkolenDvdnk <ak0lenfs@gmail.com>
2022-11-21 18:54:43 +0100

    stack tracing

--- vm.c
+++ vm.c
@@ -5,56 +5,76 @@
 
 VM vm;
 
-static void resetStack(){
+static void resetStack()
+{
     vm.stackTop = vm.stack;
 }
 
-void initVM(){
+void initVM()
+{
     resetStack();
 }
 
-void freeVM(){
-
+void freeVM()
+{
 }
 
-void push(Value value){
+void push(Value value)
+{
     *vm.stackTop = value;
     vm.stackTop++;
 }
 
-Value pop(){
+Value pop()
+{
     vm.stackTop--;
     return *vm.stackTop;
 }
 
-static InterpretResult run(){
-    #define READ_BYTE() (*vm.ip++)
-    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+static InterpretResult run()
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
-    for (;;){
+    for (;;)
+    {
 #ifdef DEBUG_TRACE_EXECUTION
-    disassembleInstruction(vm.chunk, (int)(vp.ip - vm.chunk->code));
+        printf("        ");
+
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vp.ip - vm.chunk->code));
 #endif
 
         uint8_t instruction;
-        switch (instruction = READ_BYTE()){
-            case OP_CONSTANT: {
-                Value constant = READ_CONSTANT;
-                printValue(constant);
-                printf("\n");
-                break;
-            }
-            case OP_RETURN: {
-                return INTERPRET_OK;
-            }
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT;
+            push(constant);
+            break;
+        }
+        case OP_RETURN:
+        {
+            printValue(pop());
+            printf("\n");
+            return INTERPRET_OK;
+        }
         }
     }
 
-    #undef READ_BYTE
-    #undef READ_CONSTANT
+#undef READ_BYTE
+#undef READ_CONSTANT
 }
 
-InterpretResult interpret(Chunk* chunk){
+InterpretResult interpret(Chunk *chunk)
+{
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
     return run();
