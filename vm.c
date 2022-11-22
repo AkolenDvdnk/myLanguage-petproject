d9ad77ba93eca4d7c55be03e46791f73ddafa064
AkolenDvdnk <ak0lenfs@gmail.com>
2022-11-22 12:32:48 +0100

    negate instruction

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
@@ -48,7 +49,7 @@ static InterpretResult run()
             printf(" ]");
         }
         printf("\n");
-        disassembleInstruction(vm.chunk, (int)(vp.ip - vm.chunk->code));
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
         uint8_t instruction;
@@ -56,10 +57,13 @@ static InterpretResult run()
         {
         case OP_CONSTANT:
         {
-            Value constant = READ_CONSTANT;
+            Value constant = READ_CONSTANT();
             push(constant);
             break;
         }
+        case OP_NEGATE:
+        push(-pop());
+        break;
         case OP_RETURN:
         {
             printValue(pop());
