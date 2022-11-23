d20ed004d2d3e58aab2c127dad64fa28a7c290ea
AkolenDvdnk <ak0lenfs@gmail.com>
2022-11-23 16:52:28 +0100

    small fixes

--- compiler.c
+++ compiler.c
@@ -4,6 +4,9 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source){
+void compile(const char* source, Chunk* chunk){
     initScanner(source);
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
